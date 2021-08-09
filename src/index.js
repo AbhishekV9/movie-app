c49adb3623ce8ebb5c329753a1581e77fc95f80b
abhishek <avishekv444@gmail.com>
2021-08-09 16:38:43 +0530

    Created the store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers'
 
+
+const store=createStore(movies); //creating store(redux),we need to pass an argument in createStore i.e reducers
+console.log('store',store)
 
 ReactDOM.render(
   <React.StrictMode>
